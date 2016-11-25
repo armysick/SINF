8ae9cc949ac504f2df9207a2eb7a02a78fa9e2ac
armysick <uh4v3b33nh4ck3d@gmail.com>
2016-11-25 02:10:06 +0000

    Added sinfApp [ongoing]

---
+++ sinfApp/src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {Http, RequestOptions, Headers} from '@angular/http';
+import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+  vendedor_id:string;
+  posts: any;
+  constructor(public navCtrl: NavController, public http: Http) {
+    let opt: RequestOptions
+    let myHeaders: Headers = new Headers
+    myHeaders.set('Content-type', 'application/json');
+    myHeaders.append('User', 'user');
+    myHeaders.append('Password', 'Feup2016')
+
+    opt = new RequestOptions({
+      headers: myHeaders
+    })
+    this.http.get("http://localhost:49822/api/Clientes",opt).subscribe (data => {
+      console.log("Got Data");
+      this.items = JSON.parse(data._body).people;
+
+ //http://stackoverflow.com/questions/37841949/add-header-to-the-api-http-request-angular-2-ionic-2   });
+
+    console.log(this.posts[0].data);
+  }
+  search(){
+
+  }
+
+}
